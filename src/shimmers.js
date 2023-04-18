ab25568d0bbbbaffff0fec4c9b4f62fe24ed58f8
sidhunishan786 <sidhunishan786@gmail.com>
2023-04-18 18:43:30 +0530

    css of shimmers

--- src/shimmers.js
+++ src/shimmers.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Shimmers=()=>{
     return (
-    <div >
+    <div id="shimmermain">
         <div id="shimmer">
             <h1>we are loading your data......
                 <br></br>
